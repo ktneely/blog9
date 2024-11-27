6e21d2adeb6253e43802445679f57ca7a72d1598
Kevin <ktneely@astroturfgarden.com>
2024-11-26 17:16:52 -0800

    Update config.mjs
    adding some default examples

--- content/config.mjs
+++ content/config.mjs
@@ -2,4 +2,17 @@
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
+
 export default {};
+
+const config = {
+  title: "Blog 9 from Outer Space",
+  description: "A digital garden with some flowers among all the weeds",
+  author: "Kevin",
+  defaultAuthor: "ktneely",
+  // links to the pages you want to link to in the navbar and in the footer
+  navLinks: [{ href: "/about", name: "About" }],
+};
+
+export default config;
+
