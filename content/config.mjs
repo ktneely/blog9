550a108dd3d9dd052ef98a4807cee0fe6c773284
Kevin <ktneely@astroturfgarden.com>
2024-11-26 18:44:11 -0800

    Update config.mjs
    added domain

--- content/config.mjs
+++ content/config.mjs
@@ -11,6 +11,7 @@ const config = {
   description: "A digital garden with some flowers among all the weeds",
   author: "Kevin",
   defaultAuthor: "ktneely",
+  domain: "https://blog9-zeta.vercel.app/",
   // links to the pages you want to link to in the navbar and in the footer
   navLinks: [{ href: "/blog", name: "Blog" },{ href: "/projects", name: "Projects" },{ href: "/research", name: "Research" },{ href: "/about", name: "About" }],
 };
