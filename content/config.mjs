431254c8f4d6b1b7e0044b9188d2782d65344f78
Kevin Neely <ktneely@astroturfgarden.com>
2025-01-06 15:37:59 -0800

    updating config

--- content/config.mjs
+++ content/config.mjs
@@ -7,13 +7,17 @@ export default {};
 
 
 const config = {
-  title: "Blog 9 from Outer Space",
+  title: "Blog 9 draft title from Outer Space",
   description: "A digital garden with some flowers among all the weeds",
-  author: "Kevin",
+  author: "Kevin Neely",
   defaultAuthor: "ktneely",
   domain: "https://blog9-zeta.vercel.app/",
   // links to the pages you want to link to in the navbar and in the footer
   navLinks: [{ href: "/blog", name: "Blog" },{ href: "/projects", name: "Projects" },{ href: "/research", name: "Research" },{ href: "/about", name: "About" }],
+  socialLinks: {
+    linkedin: "https://www.linkedin.com/in/kevintneely/",
+    mastodon: "https://infosec.exchange/@ktneely", 
+  }
 };
 
 export default config;
