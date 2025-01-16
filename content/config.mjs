f57202903610b7e2f7a29e281953f1b84b84c9a1
Kevin Neely <ktneely@astroturfgarden.com>
2025-01-15 22:26:46 -0800

    updating main config

--- content/config.mjs
+++ content/config.mjs
@@ -11,12 +11,22 @@ const config = {
   description: "A digital garden with some flowers among all the weeds",
   author: "Kevin Neely",
   defaultAuthor: "ktneely",
-  domain: "https://blog9-zeta.vercel.app/",
+  # domain: "https://blog9-zeta.vercel.app/",
+  domain: "https://the15thstandard.com",
   // links to the pages you want to link to in the navbar and in the footer
-  navLinks: [{ href: "/blog", name: "Blog" },{ href: "/projects", name: "Projects" },{ href: "/research", name: "Research" },{ href: "/about", name: "About" }],
+  navLinks: [
+    { href: "/blog", name: "Blog" },
+    { href: "/projects", name: "Projects" },
+    { href: "/research", name: "Research" },
+    { href: "/about", name: "About" }
+  ],
   socialLinks: {
     linkedin: "https://www.linkedin.com/in/kevintneely/",
     mastodon: "https://infosec.exchange/@ktneely", 
+  },
+  nextSeo: {
+    title: "The 15th Standard",
+    description: "This site covers cybersecurity leadership and advice regarding the building of cyber and information security programs for successful companies.",
   }
 };
 
